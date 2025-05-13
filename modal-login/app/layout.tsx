6e0627a98ab7b1f2c87a328f03e61e7587914414
zh1p4ng <yzp511966601@163.com>
2025-05-13 18:45:09 +0800

    return fix

--- modal-login/app/layout.tsx
+++ modal-login/app/layout.tsx
@@ -1,6 +1,8 @@
 import { config } from "@/config";
+import { cookieToInitialState } from "@account-kit/core";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { headers } from "next/headers";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -13,13 +15,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
+  // Persist state across pages
+  // https://accountkit.alchemy.com/react/ssr#persisting-the-account-state
+  const initialState = cookieToInitialState(
+    config,
+    headers().get("cookie") ?? undefined,
+  );
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers initialState={initialState}>{children}</Providers>
       </body>
     </html>
   );
--- modal-login/app/providers.tsx
+++ modal-login/app/providers.tsx
@@ -1,30 +1,19 @@
-'use client';
-
-import { useEffect, useState } from "react";
+"use client";
 import { config, queryClient } from "@/config";
-import { cookieToInitialState, AlchemyClientState } from "@account-kit/core";
+import { AlchemyClientState } from "@account-kit/core";
 import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-export const Providers = (props: PropsWithChildren) => {
-  const [initialState, setInitialState] = useState<AlchemyClientState>();
-
-  useEffect(() => {
-    // 只有在浏览器环境下执行，确保安全
-    const state = cookieToInitialState(config, document.cookie);
-    setInitialState(state);
-  }, []);
-
-  // 若状态未初始化则不渲染，防止 hydration 错误
-  if (!initialState) return null;
-
+export const Providers = (
+  props: PropsWithChildren<{ initialState?: AlchemyClientState }>,
+) => {
   return (
     <QueryClientProvider client={queryClient}>
       <AlchemyAccountProvider
         config={config}
         queryClient={queryClient}
-        initialState={initialState}
+        initialState={props.initialState}
       >
         {props.children}
       </AlchemyAccountProvider>
