f33e242fc2b2a5e90677354bd722a71f1b8207b7
zh1p4ng <yzp511966601@163.com>
2025-05-14 17:17:22 +0800

    Update providers.tsx

--- modal-login/app/providers.tsx
+++ modal-login/app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { config, queryClient } from "@/config";
 import { AlchemyClientState } from "@account-kit/core";
-import { AlchemyAccountProvider, Hydrate } from "@account-kit/react";
+import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
@@ -15,9 +15,7 @@ export const Providers = (
         queryClient={queryClient}
         initialState={props.initialState}
       >
-        <Hydrate state={props.initialState}>
-          {props.children}
-        </Hydrate>
+        {props.children}
       </AlchemyAccountProvider>
     </QueryClientProvider>
   );
