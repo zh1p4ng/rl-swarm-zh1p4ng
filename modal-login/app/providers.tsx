592ba0ba1ee36972734e02c77e319f8d982e27ec
zh1p4ng <yzp511966601@163.com>
2025-05-14 17:12:51 +0800

    fix : "Node doesn't start after successful login – TypeError 'hasHydrated'"
    fix : "Node doesn't start after successful login – TypeError 'hasHydrated'"

--- modal-login/app/providers.tsx
+++ modal-login/app/providers.tsx
@@ -1,30 +1,23 @@
-'use client';
-
-import { useEffect, useState } from "react";
+"use client";
 import { config, queryClient } from "@/config";
-import { cookieToInitialState, AlchemyClientState } from "@account-kit/core";
-import { AlchemyAccountProvider } from "@account-kit/react";
+import { AlchemyClientState } from "@account-kit/core";
+import { AlchemyAccountProvider, Hydrate } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-export const Providers = (props: PropsWithChildren) => {
-  const [initialState, setInitialState] = useState<AlchemyClientState | null>(null);
-
-  useEffect(() => {
-    const state = cookieToInitialState(config, document.cookie);
-    setInitialState(state);
-  }, []);
-
-  if (!initialState) return null;
-
+export const Providers = (
+  props: PropsWithChildren<{ initialState?: AlchemyClientState }>,
+) => {
   return (
     <QueryClientProvider client={queryClient}>
       <AlchemyAccountProvider
         config={config}
         queryClient={queryClient}
-        initialState={initialState}
+        initialState={props.initialState}
       >
-        {props.children}
+        <Hydrate>
+          {props.children}
+        </Hydrate>
       </AlchemyAccountProvider>
     </QueryClientProvider>
   );
